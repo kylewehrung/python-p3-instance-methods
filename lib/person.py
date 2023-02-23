ed99d0794c1f07c6c47841373bd443e0fc428d93
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-02-22 20:39:21 -0700

    not-so-hot-take: this is p fun

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,12 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+    
+greg = Person()
+greg.talk()
+greg.walk()
